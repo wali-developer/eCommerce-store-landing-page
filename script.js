1885a3ead0b9096af6da7c4fe5c370aad05dbdfb
wali-developer <91718115+wali-developer@users.noreply.github.com>
2021-10-23 11:19:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//==== Mobile Menu ====
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  /*Dropdown Menu
+  function display() {
+    var y = document.getElementsByClassName("dropdown-content");
+    if (y.style.display == "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+  }*/
